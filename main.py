5738665be1a133ad4c40ddba5b458a9439789f85
MohGanji <mohammad-ganji@outlook.com>
2018-05-11 15:53:17 +0430

    added functions with descriptions

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ class Network:
 
     input_hidden_links = []
     hidden_output_links = []
+    hidden_bias = []
+    output_bias = []
 
     def __init__(self, input_size, hidden_size, output_size):
         self.input_size= input_size
@@ -25,11 +27,44 @@ class Network:
 
         self.input_hidden_links = [ [np.random.uniform(-1, 1) for j in xrange(0, self.hidden_size)] for i in xrange(0, self.input_size)]
         self.hidden_output_links = [ [np.random.uniform(-1, 1) for k in xrange(0, self.output_size)] for j in xrange(0, self.hidden_size)]
+        self.hidden_bias = [np.random.uniform(-1, 1) for j in xrange(0, self.hidden_size)]
+        self.output_bias = [np.random.uniform(-1, 1) for k in xrange(0, self.output_size)]
 
         pprint(self.input_hidden_links)
         pprint(self.hidden_output_links)
         
-        
+    
+    def backprop(pixels, expected_output):
+        """
+            * pixels: 2 dim array of pixels of an image, each field in range(0, 255)
+            * expected_output:  one dim array with length of output layer, values: (0, 1)
+
+            * updates weight of links based on cost of output
+        """
+        pass
+
+    def train(dataset, mode):
+        """
+            * mode: sgd | gd
+                gd: gradiant descent
+                sgd: stochastic gradiant descent
+            * dataset: an object of arrays of names of png files:
+                {
+                    'a': [1.png, 2.png]
+                }
+
+            * returns: nothings
+                runs backprop on data based on mode.
+        """
+        pass
+
+    def test(input):
+        """
+            * input: a png file (or pixels, not decided yet)
+            * output: returns a character between (A, J) with a probability
+        """
+        pass
+
 def main():
     net = Network(784, 16, 10)
 
